9c15abc4cf62492f3d73e4a8ba27d0485a914304
Joao Oliveira <joaooliveira1775@gmail.com>
2022-08-19 17:51:24 -0300

    Chore/storybook (#6)
    * feat: add storybook
    
    * chore: add storybook config
    
    * feat: add stories header
    
    * feat: add a simple Title component on storybook
    
    * refactor: better way to create component storybook
    
    * chore: add build file to gitgnore

---
+++ src/components/Title/Title.stories.tsx
@@ -0,0 +1,13 @@
+import { Title } from './index'
+
+const stories = {
+  title: 'Components / Title',
+}
+
+export const RedTitle = () => <Title color="red" />
+
+export const BlueTitle = () => <Title color="blue" />
+
+export const YellowTitle = () => <Title color="yellow" />
+
+export default stories
