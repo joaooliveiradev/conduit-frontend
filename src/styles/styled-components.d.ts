09fc0c0a9aaed7c43eb9390d10394cf1ad0d9c06
Joao Oliveira <joaooliveira1775@gmail.com>
2022-08-23 19:19:27 -0300

    refactor: disable eslint rule to export empty interface

--- src/styles/styled-components.d.ts
+++ src/styles/styled-components.d.ts
@@ -5,5 +5,6 @@ import theme from './theme'
 type CustomTheme = typeof theme
 
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends CustomTheme {}
 }
