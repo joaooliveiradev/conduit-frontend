decf31a62a258771aa025f6bd12b7134a2dfd05d
Joao Oliveira <joaooliveira1775@gmail.com>
2022-08-23 19:17:44 -0300

    chore: config to resolve path alias and add theme, global styles in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,19 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import theme from '../src/styles/theme'
+import GlobalStyles from '../src/styles/global'
+
+const withThemeProvider = (Story, context) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Story {...context} />
+  </ThemeProvider>
+)
+
+export const decorators = [withThemeProvider]
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
