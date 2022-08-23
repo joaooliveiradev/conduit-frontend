fc537e1646900ec362747f25ceac374a1fb07836
Joao Oliveira <joaooliveira1775@gmail.com>
2022-08-23 18:15:06 -0300

    feat: add styled components theme provider to the app

---
+++ src/styles/styled-components.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components'
+
+import theme from './theme'
+
+type CustomTheme = typeof theme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends CustomTheme {}
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+export default {
+  fonts: {
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      xmedium: '1.8rem',
+      large: '2.0rem',
+      xlarge: '2.2rem',
+      xxlarge: '2.4rem',
+      huge: '3.2rem',
+      xhuge: '4.2rem',
+    },
+  },
+  spacings: {
+    xsmall: '0.8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.2rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
+  colors: {
+    white: '#FAFAFA',
+    lightGrey: '#B1B1B1',
+    grey: '#777777',
+    darkGrey: '#4F4F4F',
+    lightBlack: '#2C2C2C',
+    black: '#1D1D1D',
+    darkBlack: '#171717',
+  },
+} as const
