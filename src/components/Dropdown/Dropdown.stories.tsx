87104e8bee71658dbcacfba7ad703e4466690727
Joao Oliveira <joaooliveira1775@gmail.com>
2022-09-08 09:42:33 -0300

    Feat/dropdown (#17)
    * feat: add radix popover component
    
    * feat: add dropdown and dropdown item component
    
    * fix: fix some appointments on pull request

---
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,37 @@
+import ProfileName from '@components/ProfileName'
+import { Meta, Story } from '@storybook/react'
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownProps,
+  DropdownItemProps,
+} from './index'
+
+type DropdownStoriesProp = Omit<DropdownProps, 'children'> & DropdownItemProps
+
+const stories: Meta<DropdownStoriesProp> = {
+  component: Dropdown,
+  argTypes: {
+    trigger: {
+      table: {
+        disable: true,
+      },
+    },
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+export default stories
+
+export const Template: Story = () => {
+  return (
+    <Dropdown trigger={<ProfileName size={4} name="João" />}>
+      <DropdownItem href="/profile" label="Profiles" />
+      <DropdownItem href="/signout" label="Sign out" />
+    </Dropdown>
+  )
+}
