d995ae98505810b77da04aabb474e3a3a67e79df
João Oliveira <joaooliveira1775@gmail.com>
2022-10-11 14:58:40 -0300

    feat: add react query config and hydration config to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import theme from '../src/styles/theme'
 import GlobalStyles from '../src/styles/global'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
-const withThemeProvider = (Story, context) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <Story {...context} />
-  </ThemeProvider>
-)
-
+const withThemeProvider = (Story, context) => {
+  const [queryClient] = useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Story {...context} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
+}
 export const decorators = [withThemeProvider]
 
 export const parameters = {
