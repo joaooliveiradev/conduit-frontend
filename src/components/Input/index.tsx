be64883274c94670fd358679dbe0bfc5f63d60f1
João Oliveira <joaooliveira1775@gmail.com>
2022-11-13 09:36:08 -0300

    refactor: change the color of the input to red when have an error

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -23,14 +23,16 @@ const ErrorMessage = styled.p`
 `
 
 const InputWrapper = styled.input<InputProps>`
-  ${({ theme, errorMessage }) => css`
+  ${({ theme, errorMessage, touched }) => css`
     height: ${theme.spacings.xlarge};
     border-radius: 4px;
     background-color: ${transparentize(0.88, theme.colors.black[200])};
     padding-left: ${theme.spacings.small};
     font-size: ${theme.fonts.sizes.xmedium};
     font-weight: 600;
-    color: ${errorMessage  ? theme.colors.red[100] : theme.colors.black[200]};
+    color: ${errorMessage && touched
+      ? theme.colors.red[100]
+      : theme.colors.black[200]};
     ::placeholder {
       color: ${theme.colors.grey[200]};
     }
@@ -40,9 +42,13 @@ const InputWrapper = styled.input<InputProps>`
 export const Input = ({ errorMessage, touched, ...rest }: InputProps) => {
   return (
     <Wrapper>
-      <InputWrapper {...rest} autoComplete="off" />
+      <InputWrapper
+        errorMessage={errorMessage}
+        touched={touched}
+        {...rest}
+        autoComplete="off"
+      />
       {errorMessage && touched && <ErrorMessage>{errorMessage}</ErrorMessage>}
     </Wrapper>
   )
 }
-
