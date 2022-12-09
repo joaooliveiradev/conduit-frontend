4120e1188117ab096de7a6b16ffb9a3bddb850b8
João Oliveira <joaooliveira1775@gmail.com>
2022-12-09 15:55:29 -0300

    feat: add handle with invalidUser on useMe hook to logout in case of expiration of token

--- src/hooks/queries/useMe.ts
+++ src/hooks/queries/useMe.ts
@@ -4,6 +4,7 @@ import { DefaultError, fetcher } from '@/utils'
 import { useQuery } from '@tanstack/react-query'
 import { useAuth } from '@/context/auth'
 import * as t from 'io-ts'
+import { handleInvalidUser } from '@/utils/user'
 
 const UseMeResponseCodec = t.type({
   user: UserTypeCodec,
@@ -11,19 +12,19 @@ const UseMeResponseCodec = t.type({
 
 type UseMeResponse = t.TypeOf<typeof UseMeResponseCodec>
 
-type UseMeOutput = t.OutputOf<typeof UseMeResponseCodec>
+export type UseMeOutput = t.OutputOf<typeof UseMeResponseCodec>
 
 const getMe = async () =>
   await fetcher<undefined, UseMeResponse>('/user', UseMeResponseCodec)
 
 const useMe = () => {
   const { status } = useAuth()
-
   return useQuery<Either<DefaultError, UseMeOutput>, DefaultError>(
     ['use-me'],
     getMe,
     {
       enabled: status === 'loggedIn',
+      onError: (error) => handleInvalidUser(error),
     }
   )
 }
