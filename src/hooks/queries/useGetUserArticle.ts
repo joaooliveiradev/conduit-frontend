96faba2ffb3f6c43ba1bda72a1a49a65d6485c03
João Oliveira <joaooliveira1775@gmail.com>
2022-12-09 16:13:48 -0300

    feat: add the complete implementation of get user articles which options

--- src/hooks/queries/useGetUserArticle.ts
+++ src/hooks/queries/useGetUserArticle.ts
@@ -1,6 +1,9 @@
 import { EventCodec } from '@/types/article'
 import { DefaultError, fetcher } from '@/utils'
-import { useInfiniteQuery } from '@tanstack/react-query'
+import {
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+} from '@tanstack/react-query'
 import { withMessage } from 'io-ts-types'
 import * as t from 'io-ts'
 import { Either } from 'fp-ts/Either'
@@ -17,16 +20,27 @@ type GetUserArticleResponse = t.TypeOf<typeof GetUserArticleResponseCodec>
 
 type GetUserArticleOutput = t.OutputOf<typeof GetUserArticleResponseCodec>
 
+const oneMinute = 60 * 1000
+
 export const GET_USER_ARTICLES_KEY = 'get-user-articles'
 
-export const getUserArticles = async (author?: string) =>
+type GetUserArticlesOptions = UseInfiniteQueryOptions<
+  Either<DefaultError, GetUserArticleOutput>,
+  DefaultError
+>
+
+export const getUserArticles = async (author: string) =>
   await fetcher<undefined, GetUserArticleResponse>(
     `/articles?author=${author}`,
     GetUserArticleResponseCodec
   )
 
-export const useGetUserArticles = (author?: string) =>
+export const  useGetUserArticles = (
+  author: string,
+  options: GetUserArticlesOptions
+) =>
   useInfiniteQuery<Either<DefaultError, GetUserArticleOutput>, DefaultError>(
-    [GET_USER_ARTICLES_KEY, author],
-    async () => await getUserArticles(author)
+    [GET_USER_ARTICLES_KEY],
+    async () => await getUserArticles(author),
+    { ...options, staleTime: oneMinute, retry: 3, refetchOnWindowFocus: false, refetchInterval: oneMinute }
   )
