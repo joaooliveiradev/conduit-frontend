84f38fd10f4aa3b3952becfc187ce9be5284b77d
João Oliveira <joaooliveira1775@gmail.com>
2023-01-05 17:19:47 -0300

    feat: add useGetArticle hook to fetch articles by slug

---
+++ src/hooks/queries/useGetArticle.ts
@@ -0,0 +1,37 @@
+import { DefaultError, fetcher } from '@/libs'
+import { ArticleBySlugCodec } from '@/types/article'
+import { useQuery, type UseQueryOptions } from '@tanstack/react-query'
+import { Either } from 'fp-ts/Either'
+import * as t from 'io-ts'
+
+const GetArticleCodec = t.type({
+  article: ArticleBySlugCodec,
+})
+
+type GetArticleResponse = t.TypeOf<typeof GetArticleCodec>
+
+type GetArticlesOptions = UseQueryOptions<
+  Either<DefaultError, GetArticleResponse>,
+  DefaultError
+>
+
+export const GET_ARTICLE_KEY = 'get-article-by-slug'
+
+export const getArticle = async (slug: string) =>
+  await fetcher<undefined, GetArticleResponse>(
+    `/articles/${slug}`,
+    GetArticleCodec
+  )
+
+export const useGetArticle = (slug: string, options?: GetArticlesOptions) =>
+  useQuery<Either<DefaultError, GetArticleResponse>, DefaultError>(
+    [GET_ARTICLE_KEY],
+    async () => await getArticle(slug),
+    {
+      ...options,
+      retry: false,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    }
+  )
