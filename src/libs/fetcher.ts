f45058642e94e9f41568070b75de08e8600e2c05
João Oliveira <joaooliveira1775@gmail.com>
2023-01-07 19:33:10 -0300

    refactor: delete useless file and receive the url path declaring them

--- src/libs/fetcher.ts
+++ src/libs/fetcher.ts
@@ -4,10 +4,11 @@ import {
   DefaultError,
   handleFetcherErrors,
   UnknownError,
-  baseURL,
   validateCodec,
 } from '@/libs'
 import * as t from 'io-ts'
+
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL
 
 export const fetcher = async <D, A>(
   path: string,
