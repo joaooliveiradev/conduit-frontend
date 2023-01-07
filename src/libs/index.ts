867b1db319d03d347e6452ba90e2bd3fbb58b2ff
João Oliveira <joaooliveira1775@gmail.com>
2023-01-07 19:38:19 -0300

    remove the delete file from the exports named file from the libs directory

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,6 +1,5 @@
 export * from '@/libs/calculateTotalArticles'
 export * from '@/libs/cookies'
-export * from '@/libs/env-variables'
 export * from '@/libs/errors'
 export * from '@/libs/expression'
 export * from '@/libs/fetcher'
