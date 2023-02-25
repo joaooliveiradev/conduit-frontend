1e6445f9261cf7c894862dbfd28151e267a8098d
João Oliveira <joaooliveira1775@gmail.com>
2023-02-25 08:49:05 -0300

    fix,feat(Profile): resolve some styles issues and add a toTagName function in ProfileAuthor

--- src/components/ProfileAuthor/index.tsx
+++ src/components/ProfileAuthor/index.tsx
@@ -8,6 +8,7 @@ const Wrapper = styled.div`
   ${({ theme }) => css`
     display: flex;
     flex-direction: column;
+    align-items: center;
     gap: ${theme.spacings.small};
   `}
 `
@@ -18,6 +19,7 @@ const TagName = styled.h2`
     font-size: ${theme.fonts.sizes.xmedium};
     line-height: 28px;
     font-weight: 700;
+    letter-spacing: -0.02em;
   `}
 `
 
@@ -30,9 +32,13 @@ const Name = styled.h1`
   `}
 `
 
-export const ProfileAuthor = ({ author }: ProfileAuthorProps) => (
-  <Wrapper>
-    <TagName>{`@${author}`}</TagName>
-    <Name>{author}</Name>
-  </Wrapper>
-)
+export const ProfileAuthor = ({ author }: ProfileAuthorProps) => {
+  const toTagName = (str: string) => `@${str.toLowerCase().split(' ').join('')}`
+
+  return (
+    <Wrapper>
+      <TagName>{toTagName(author)}</TagName>
+      <Name>{author}</Name>
+    </Wrapper>
+  )
+}
