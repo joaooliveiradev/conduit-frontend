01e34e583c14e0ae7a9982832021d211cde3c805
João Oliveira <joaooliveira1775@gmail.com>
2023-02-27 09:24:26 -0300

    feat(Profile): add useProfile hook to profile page

---
+++ src/hooks/queries/useProfile.ts
@@ -0,0 +1,33 @@
+import * as t from 'io-ts'
+import { ProfileCodec } from '@/types/profile'
+import { DefaultError, fetcher } from '@/libs'
+import { useQuery } from '@tanstack/react-query'
+import { Either } from 'fp-ts/Either'
+
+const UseProfileResponseCodec = t.type({
+  profile: ProfileCodec,
+})
+
+type UseProfileResponse = t.TypeOf<typeof UseProfileResponseCodec>
+
+type UseProfileOutput = t.OutputOf<typeof UseProfileResponseCodec>
+
+export const GET_PROFILE_KEY = 'get-profile'
+
+export const getProfile = async (user: string) =>
+  await fetcher<undefined, UseProfileResponse>(
+    `/profiles/${user}`,
+    UseProfileResponseCodec
+  )
+
+export const useProfile = (user: string) =>
+  useQuery<Either<DefaultError, UseProfileOutput>>(
+    [GET_PROFILE_KEY],
+    async () => await getProfile(user),
+    {
+      retry: false,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    }
+  )
