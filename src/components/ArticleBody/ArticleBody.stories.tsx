2ab2804098b8c662d72364ffe7303c1753add946
João Oliveira <joaooliveira1775@gmail.com>
2023-03-15 16:50:50 -0300

    refactor(Profile): adding type in some imports

--- src/components/ArticleBody/ArticleBody.stories.tsx
+++ src/components/ArticleBody/ArticleBody.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, Story } from '@storybook/react'
+import type { Meta, Story } from '@storybook/react'
 import styled from 'styled-components'
 import { ArticleBody, type ArticleBodyProps } from './ArticleBody'
 
