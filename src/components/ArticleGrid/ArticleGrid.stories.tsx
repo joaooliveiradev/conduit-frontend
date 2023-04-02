9d4d400bf8d400152899ea563d10e45a8d8106bf
João Oliveira <joaooliveira1775@gmail.com>
2023-04-02 10:43:33 -0300

    feat(NewArticle): adding some mock wrapping the article grid stories to better visualization

--- src/components/ArticleGrid/ArticleGrid.stories.tsx
+++ src/components/ArticleGrid/ArticleGrid.stories.tsx
@@ -6,6 +6,7 @@ import {
   ProfileName,
   type ArticleGridProps,
 } from '@/components'
+import styled from 'styled-components'
 
 const stories: Meta<ArticleGridProps> = {
   component: ArticleGrid,
@@ -50,29 +51,47 @@ const articles = [
     description:
       'After The New York Times published its extensive report on the history of Haiti’s impoverishment at the hands.',
   },
+  {
+    slug: 'bla-foo-5',
+    title: 'Bla Foo 5',
+    author: 'Jeff Jarvis',
+    date: 'May 26, 2022',
+    readTime: '3min',
+    description:
+      'After The New York Times published its extensive report on the history of Haiti’s impoverishment at the hands.',
+  },
 ]
 
+const MockComponent = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 export const Default: Story<ArticleGridProps> = () => (
-  <ArticleGrid>
-    {articles.map((article) => (
-      <ArticleCard key={article.slug}>
-        <ArticleCard.Anchor href={`/article/${article.slug}`}>
-          <ArticleCard.Main>
-            <header>
-              <ArticleCard.Title>{article.title}</ArticleCard.Title>
-            </header>
-            <section>
-              <ArticleCard.Text>{article.description}</ArticleCard.Text>
-            </section>
-          </ArticleCard.Main>
-        </ArticleCard.Anchor>
-        <ArticleCard.Footer>
-          <ArticleCard.Anchor href={`/profile/${article.author}`}>
-            <ProfileName name={article.author} size={2} />
+  <MockComponent>
+    <ArticleGrid>
+      {articles.map((article) => (
+        <ArticleCard key={article.slug}>
+          <ArticleCard.Anchor href={`/article/${article.slug}`}>
+            <ArticleCard.Main>
+              <header>
+                <ArticleCard.Title>{article.title}</ArticleCard.Title>
+              </header>
+              <section>
+                <ArticleCard.Text>{article.description}</ArticleCard.Text>
+              </section>
+            </ArticleCard.Main>
           </ArticleCard.Anchor>
-          <ArticleStats date={article.date} readTime={article.readTime} />
-        </ArticleCard.Footer>
-      </ArticleCard>
-    ))}
-  </ArticleGrid>
+          <ArticleCard.Footer>
+            <ArticleCard.Anchor href={`/profile/${article.author}`}>
+              <ProfileName name={article.author} size={2} />
+            </ArticleCard.Anchor>
+            <ArticleStats date={article.date} readTime={article.readTime} />
+          </ArticleCard.Footer>
+        </ArticleCard>
+      ))}
+    </ArticleGrid>
+  </MockComponent>
 )
