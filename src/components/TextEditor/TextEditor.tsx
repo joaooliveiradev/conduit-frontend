7ca7e4f2787f05c6beb8832e1ece97d64417a729
João Oliveira <joaooliveira1775@gmail.com>
2023-04-09 17:36:59 -0300

    feat(NewArticle): adding validation to empty text

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -49,10 +49,30 @@ const TabsPaneFullScreen = styled(TabsPane)`
 const Preview = styled(ArticleBody)`
   ${({ theme }) => css`
     padding: ${theme.spacings.xsmall} ${theme.spacings.xxsmall};
-    user-select: none;
-    min-height: 230px;
   `}
 `
+
+const EmptyPreviewWrapper = styled.section`
+  min-height: 230px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const EmptyPreviewText = styled.h1`
+  ${({ theme }) => css`
+    font-size: ${theme.fonts.sizes.xlarge};
+    color: ${theme.colors.grey[100]};
+  `}
+`
+
+const EmptyPreview = () => {
+  return (
+    <EmptyPreviewWrapper>
+      <EmptyPreviewText>Nothing to preview.</EmptyPreviewText>
+    </EmptyPreviewWrapper>
+  )
+}
 
 export const TextEditor = () => {
   const [text, setText] = useState<string>('')
@@ -85,14 +105,14 @@ export const TextEditor = () => {
           </TabContent>
           <TabContent value="preview">
             {text.length === 0 ? (
-              <h1>Nothing to preview </h1>
+              <EmptyPreview />
             ) : (
               <Preview articleText={text} />
             )}
           </TabContent>
           <TabContent value="previewFullScreen">
             {text.length === 0 ? (
-              <h1>Nothing to preview </h1>
+              <EmptyPreview />
             ) : (
               <Preview articleText={text} />
             )}
