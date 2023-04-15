c3c2c34f9ac6c9de70dff58a19a0b991ac3317ca
João Oliveira <joaooliveira1775@gmail.com>
2023-04-15 09:58:17 -0300

    feat(NewArticle): adding close component and examples with uncontrolled and controlled in stories of alert component

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -1,16 +1,17 @@
 import type { Story, Meta } from '@storybook/react'
 import { Alert, type AlertProps, TextButton } from '@/components'
 import styled from 'styled-components'
+import { useState } from 'react'
 
 const stories: Meta<AlertProps> = {
   component: Alert,
   argTypes: {
     status: {
       table: {
-        disable: true
-      }
-    }
-  }
+        disable: true,
+      },
+    },
+  },
 }
 
 export default stories
@@ -20,15 +21,19 @@ const WrapperContent = styled.div`
   gap: ${({ theme }) => theme.spacings.xsmall};
 `
 
-export const SuccessAlert: Story<AlertProps> = () => (
-  <Alert status="success">
-    <WrapperContent>
-      <Alert.Icon />
-      <Alert.Text>Your article has been successfully created.</Alert.Text>
-      <TextButton href="/article/test">View</TextButton>
-    </WrapperContent>
-  </Alert>
-)
+export const SuccessAlert: Story<AlertProps> = () => {
+  const [open, setIsOpen] = useState(true)
+  return (
+    <Alert status="success" open={open} onOpenChange={setIsOpen}>
+      <WrapperContent>
+        <Alert.Icon />
+        <Alert.Text>Your article has been successfully created.</Alert.Text>
+        <TextButton href="/article/test">View</TextButton>
+        <Alert.Close />
+      </WrapperContent>
+    </Alert>
+  )
+}
 
 export const ErrorAlert: Story<AlertProps> = () => (
   <Alert status="error">
@@ -37,6 +42,7 @@ export const ErrorAlert: Story<AlertProps> = () => (
       <Alert.Text>
         Something unexpected happened. Please try again later.
       </Alert.Text>
+      <Alert.Close />
     </WrapperContent>
   </Alert>
 )
