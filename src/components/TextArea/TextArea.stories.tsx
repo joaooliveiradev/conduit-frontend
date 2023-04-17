e35fc330d27067c77df2a30c2f0ccf2d748b68e7
João Oliveira <joaooliveira1775@gmail.com>
2023-04-17 13:21:17 -0300

    feat(NewArticle): adding more states and stories in text area component.

--- src/components/TextArea/TextArea.stories.tsx
+++ src/components/TextArea/TextArea.stories.tsx
@@ -14,6 +14,11 @@ const stories: Meta<TextAreaProps> = {
         disable: true,
       },
     },
+    className: {
+      table: {
+        disable: true,
+      },
+    },
   },
 }
 
@@ -21,7 +26,19 @@ export default stories
 
 const Template: Story<TextAreaProps> = (args) => <TextArea {...args} />
 
-export const Default = Template.bind({})
-Default.args = {
+export const Empty = Template.bind({})
+Empty.args = {
+  placeholder: 'Type something awesome!',
+}
+
+export const Filled = Template.bind({})
+Filled.args = {
   children: 'Content',
+}
+
+export const Errored = Template.bind({})
+Errored.args = {
+  errorMessage: 'Some error message.',
+  touched: true,
+  defaultValue: 'Some value.',
 }
