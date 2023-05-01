f20012dd8cc1e4cb27799de33dde440a9605cc12
João Oliveira <joaooliveira1775@gmail.com>
2023-05-01 09:55:16 -0300

    feat(NewArticle): adding a more clear implementation of highlight in article body

--- src/components/ArticleBody/ArticleBody.tsx
+++ src/components/ArticleBody/ArticleBody.tsx
@@ -1,24 +1,15 @@
+import rehypeSanitize, {
+  defaultSchema,
+  type Options as RehypeSanitizeOptions,
+} from 'rehype-sanitize'
 import styled, { css } from 'styled-components'
-import rehypeSanitize from 'rehype-sanitize'
-import nightOwl from 'react-syntax-highlighter/dist/cjs/styles/prism/night-owl'
-import dynamic from 'next/dynamic'
-import ReactMarkdown from 'react-markdown'
-import type { SyntaxHighlighterProps } from 'react-syntax-highlighter'
-import type { ReactNode } from 'react'
-
-const SyntaxHighlighter = dynamic<SyntaxHighlighterProps>(
-  () =>
-    import('react-syntax-highlighter').then((module) => module.PrismAsyncLight),
-  { ssr: false }
-)
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import { default as rehypeHighlight } from 'rehype-highlight/lib/index'
+import 'highlight.js/styles/night-owl.css'
 
 export type ArticleBodyProps = {
   articleText: string
 }
-
-type CodeProps = {
-  children: ReactNode & ReactNode[]
-} & Omit<SyntaxHighlighterProps, 'children'>
 
 const Wrapper = styled.section`
   ${({ theme }) => css`
@@ -78,32 +69,25 @@ const Wrapper = styled.section`
   `}
 `
 
-const CodeHighlighter = styled(SyntaxHighlighter)`
-  border-radius: ${({ theme }) => theme.spacings.small};
-`
-
-const Code = ({ children }: CodeProps) => {
-  const defaultLanguage = 'javascript'
-  return (
-    <CodeHighlighter
-      style={nightOwl}
-      language={defaultLanguage}
-      showLineNumbers
-      wrapLongLines
-    >
-      {String(children)}
-    </CodeHighlighter>
-  )
+const rehypeSanitizeSettings: RehypeSanitizeOptions = {
+  ...defaultSchema,
+  attributes: {
+    ...defaultSchema.attributes,
+    code: [
+      ...((defaultSchema.attributes && defaultSchema.attributes.code) || []),
+      ['className', 'language-js', 'hljs-js', 'language-md'],
+    ],
+  },
 }
 
 export const ArticleBody = ({ articleText, ...rest }: ArticleBodyProps) => {
   return (
     <Wrapper {...rest}>
       <ReactMarkdown
-        rehypePlugins={[rehypeSanitize]}
-        components={{
-          code: ({ children }) => <Code>{children}</Code>,
-        }}
+        rehypePlugins={[
+          [rehypeSanitize, rehypeSanitizeSettings],
+          [rehypeHighlight],
+        ]}
       >
         {articleText}
       </ReactMarkdown>
