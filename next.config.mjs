67ea7e19bc7e50aa689f4e5418ad319a9bd7fefb
João Oliveira <joaooliveira1775@gmail.com>
2023-05-03 17:59:12 -0300

    feat(NewArticle): adding a basic security headers on next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,45 @@
 /** @type {import('next').NextConfig} */
 import withBundleAnalyzer from '@next/bundle-analyzer'
 
+const ContentSecurityPolicy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    child-src 'none';
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    font-src 'self';
+    connect-src *;
+`
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Controls',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+]
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     styledComponents: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
   },
   webpack: (config) => {
     const svgrConfig = {
