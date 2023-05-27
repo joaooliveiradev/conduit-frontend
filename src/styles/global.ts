73a4b1e971b14f91f1fe7048e5f8734726105c65
João Oliveira <joaooliveira1775@gmail.com>
2023-05-27 13:44:40 -0300

    feat(NewArticle): adding scroll behaviour smooth

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,37 +2,37 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
     * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      scroll-behavior: smooth;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
       &::before,
       &::after {
         box-sizing: inherit;
       }
-  }
-  *,
-  button,
-  input {
-    border: 0;
-    font-family: 'DM Sans', sans-serif;
-  }
-  html,
-  body {
-    font-size: 62.5% ;
-    font-family: 'DM Sans', sans-serif;
-    background-color: #FAFAFA;
-    a {
-      text-decoration: none;
-      color: inherit;
-      :hover {
+    }
+    *,
+    button,
+    input {
+      border: 0;
+    }
+    html,
+    body {
+      font-size: 62.5%;
+      font-family: 'DM Sans', sans-serif;
+      a {
+        text-decoration: none;
         color: inherit;
+        :hover {
+          color: inherit;
+        }
       }
     }
-  }
-  #__next {
-    min-height: 100vh;
-    display: flex;
+    #__next {
+      min-height: 100vh;
+      display: flex;
+      background-color: #FAFAFA;
   }
 `
