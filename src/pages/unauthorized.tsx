d6ec74e27425f1b275b5e18ab3c63d0e76c3c325
João Oliveira <joaooliveira1775@gmail.com>
2023-06-05 14:34:09 -0300

    feat(NewArticle): adding a basic implementation of unauthorized page.

---
+++ src/pages/unauthorized.tsx
@@ -0,0 +1,91 @@
+import { Button, ErrorStateIcon, SignInModal } from '@/components'
+import { useAuth } from '@/context'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  row-gap: ${({ theme }) => theme.spacings.large};
+  align-items: center;
+`
+
+const Title = styled.h1`
+  font-size: ${({ theme }) => theme.fonts.sizes.xxlarge};
+`
+
+const ErrorDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacings.xxsmall};
+  align-items: center;
+`
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacings.small};
+  align-items: center;
+`
+
+const Text = styled.p`
+  font-size: ${({ theme }) => theme.fonts.sizes.xmedium};
+  color: ${({ theme }) => theme.colors.grey['300']};
+  font-weight: 500;
+`
+
+const BoldText = styled.b`
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.black['400']};
+`
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: ${({ theme }) => theme.spacings.large};
+`
+
+const Unauthorized = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+  const { status } = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'loggedIn') {
+      router.replace('/editor')
+      setIsModalOpen(false)
+    }
+  }, [status, router])
+
+  return (
+    <Wrapper>
+      <ErrorStateIcon />
+      <ErrorDescription>
+        <Title>Hold up! Error 401</Title>
+        <TextWrapper>
+          <Text>This page is for signed-in users only.</Text>
+          <Text>
+            Please return to the <BoldText>homepage</BoldText> or{' '}
+            <BoldText>sign in</BoldText>.
+          </Text>
+        </TextWrapper>
+      </ErrorDescription>
+      <ButtonWrapper>
+        <Link href="/" replace prefetch={false}>
+          <Button size="large">Back to home</Button>
+        </Link>
+        <Button size="large" onClick={() => setIsModalOpen(true)}>
+          Sign in
+        </Button>
+        <SignInModal
+          open={isModalOpen}
+          onOpenChange={(open) => setIsModalOpen(open)}
+          showSignInFirst
+        />
+      </ButtonWrapper>
+    </Wrapper>
+  )
+}
+
+export default Unauthorized
