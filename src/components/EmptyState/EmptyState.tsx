dca1f2b4737b9985172e564ce8757510e831e97d
João Oliveira <joaooliveira1775@gmail.com>
2023-07-20 08:41:32 -0300

    refactor(NewArticle): changing EmptyState component to be compound

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -1,9 +1,7 @@
-import { EmptyIcon } from '@/components'
 import styled from 'styled-components'
 
 export type EmptyStateProps = {
-  message: string
-  title: string
+  children: React.ReactNode
 }
 
 const Wrapper = styled.div`
@@ -13,31 +11,24 @@ const Wrapper = styled.div`
   row-gap: ${({ theme }) => theme.spacings.large};
 `
 
-const TextWrapper = styled.div`
+export const EmptyStateContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   row-gap: ${({ theme }) => theme.spacings.xsmall};
 `
 
-const Title = styled.h1`
+export const EmptyStateTitle = styled.h1`
   font-size: ${({ theme }) => theme.fonts.sizes.xxlarge};
 `
 
-const Description = styled.p`
+export const EmptyStateDescription = styled.p`
   font-size: ${({ theme }) => theme.fonts.sizes.xmedium};
   color: ${({ theme }) => theme.colors.grey[300]};
   font-weight: 500;
 `
 
-export const EmptyState = ({ message, title }: EmptyStateProps) => {
-  return (
-    <Wrapper>
-      <EmptyIcon />
-      <TextWrapper>
-        <Title>{title}</Title>
-        <Description>{message}</Description>
-      </TextWrapper>
-    </Wrapper>
-  )
-}
+export const EmptyState = ({ children }: EmptyStateProps) => (
+  <Wrapper>{children}</Wrapper>
+)
+
